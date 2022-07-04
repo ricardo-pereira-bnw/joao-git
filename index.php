9ec00b412835c2649db757432cb512366e6470ae
Ricardo Pereira <ricardo.pereira@buenonetworks.com.br>
2022-07-04 15:10:23 -0300

    Resolucao de conflitos

--- index.php
+++ index.php
@@ -3,10 +3,5 @@
 declare(strict_types=1);
 
 echo "João é legal pra caramba!";
-<<<<<<< HEAD
-echo "Ricardo achou o joão profissional e bacana";
-echo "A BNW é chata";
-=======
 echo "Ricardo achou o joão profissional";
 echo "A BNW é leve de trabalhar";
->>>>>>> 8890
